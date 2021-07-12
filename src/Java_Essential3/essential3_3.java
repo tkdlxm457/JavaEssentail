74817517bd73358fc499392ed9e1b6fa831640e0
tkdlxm457 <tkdlxm457@naver.com>
2021-07-12 13:02:56 +0900

    3장 실습문제

--- src/Java_Essential3/essential3_3.java
+++ src/Java_Essential3/essential3_3.java
@@ -1,10 +1,32 @@
 package Java_Essential3;
+import java.util.Scanner;
 
 public class essential3_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int [] a = new int [10];
+		int temp = 0;
+		
+		System.out.print("정수 10개를 입력하세요>>");
+		
+		for(int i = 0; i < a.length; i++) {
+			a[i] = scanner.nextInt();
+		}
+		
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a.length-i-1; j++) {
+				if(a[j] > a[j+1]) {
+					temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+		for(int i = 0; i < 10; i++) {
+			System.out.print(a[i] + " ");
+		}
 	}
 
 
--- src/Java_Essential3/essential3_4.java
+++ src/Java_Essential3/essential3_4.java
@@ -1,11 +1,11 @@
 package Java_Essential3;
+import java.util.Scanner;
 
 public class essential3_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		String str [] = {"가위", "바위", "보"};
+		
+		}
 	}
-
-
-}
