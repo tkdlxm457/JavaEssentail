6d1aef7c8904e16dc5bcad16b18c43c73a06ae7f
tkdlxm457 <tkdlxm457@naver.com>
2021-07-22 20:04:46 +0900

    5장실습문제

---
+++ src/Java_Essential5/App.java
@@ -0,0 +1,43 @@
+package Java_Essential5;
+import java.util.*;
+
+abstract class Calculator{
+	protected int a, b;
+	abstract protected int calc();
+	protected void input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 2개를 입력하세요>>");
+		a = sc.nextInt();
+		b = sc.nextInt();
+	}
+	public void run() {
+		input();
+		int res = calc();
+		System.out.println("계산된 값은" + res);
+	}
+}
+
+class Adder extends Calculator{
+		protected int calc() {
+				return a + b;
+		}
+}
+
+class Subtracter extends Calculator{
+	protected int calc() {
+			return a - b;
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		Adder adder = new Adder();
+		Subtracter sub = new Subtracter();
+		
+		adder.run();
+		sub.run();
+
+	}
+
+}
---
+++ src/Java_Essential5/StackManager.java
@@ -0,0 +1,10 @@
+package Java_Essential5;
+
+public class StackManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
